b65e2d66d555e9b565e2e10fc8d2a70e0fc26bd3
OoMorizono <x65f588d72sw76@gmail.com>
2021-11-07 11:10:19 +0900

    機能の追加

--- ZipCodeApp/ViewController.swift
+++ ZipCodeApp/ViewController.swift
@@ -23,6 +23,15 @@ import UIKit
      let kana3: String
      let prefcode: String
      let zipcode: String
+     
+     
+     func address() -> String {
+         return address1 + address2 + address3
+     }
+     
+     func kana() -> String{
+         return kana1 + kana2 + kana3
+     }
  }
 
 
@@ -43,6 +52,9 @@ class ViewController: UIViewController {
 
     }
     
+    
+    
+    
          // MARK: リクエストメソッド
     func requestAddressFromZipCode(zipCode: String) {
     
@@ -57,22 +69,50 @@ class ViewController: UIViewController {
                  do {
                      let decoder = JSONDecoder()
                      responseData = try decoder.decode(ZipCloudResponse.self, from: data)
+                     print(responseData?.results)
                  } catch {
                      // do{ }でtryが失敗した場合の処理
                      print(error.localizedDescription)
                  }
                  // 住所情報が合ったら保存
                  if responseData?.results != nil {
-                     self.results = (responseData?.results)!  // 取得したdataのresultsを
+                     for result in
+                            (responseData?.results)! {
+                         self.results.append(result)
+                     }
                  }
                  
-                 // TableViewを再表示
                  DispatchQueue.main.async {
+                // 取得したデータから住所情報を取得
+                if responseData?.results != nil {
+                    self.results = (responseData?.results)!
+                } else {
+                    self.showAlert(title: "エラー！", message: "存在しない番号です")
+                }
                      self.addressTableView.reloadData()
-                 }
+                }
                  
              }
              task.resume()
+         }
+    
+        //正規表現のチェックのためのメソッド(数字で7文字)
+         func isZipCode(enteredText: String) -> Bool {
+             let pattern = "^[0-9]{7}$"                                                            // パターンを作成
+             guard let regex = try? NSRegularExpression(pattern: pattern) else { return false }    // NSRegularExpressionをインスタンス化
+             let matches = regex.matches(
+                in: enteredText,
+                range: NSRange(location:0,
+                               length:enteredText.count))    // パターンマッチを確認
+             return matches.count == 1 ? true : false                                              // 結果によって真偽値を返す
+         }
+    
+    //アラート表示のメソッド
+         func showAlert(title: String, message: String) {
+             let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) // 表示作成
+             let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)           // ボタン作成
+             alert.addAction(alertAction)                                                          // 表示にボタンを追加
+             present(alert, animated: true)                                                        // 画面に表示
          }
 
 }
@@ -80,14 +120,22 @@ class ViewController: UIViewController {
 // MARK: delegate
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //入力したらキーボードを下げる｡
+        searchBar.resignFirstResponder()
         guard let searchText = searchBar.text else { return }
-                requestAddressFromZipCode(zipCode: searchText)
-            }
+        guard isZipCode(enteredText: searchText)
+        else{
+            showAlert(title: "エラー!", message: "数字7文字を入力してください;")
+            return
+        }
+        requestAddressFromZipCode(zipCode: searchText)
+        
+        }
     }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return results.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -95,15 +143,8 @@ extension ViewController: UITableViewDataSource {
             .dequeueReusableCell(withIdentifier:"AddressCell",for: indexPath)
         
             let address = results[indexPath.row]
-            cell.textLabel?.text =
-            address.address1
-            + address.address2
-            + address.address3
-            + "("
-            + address.kana1
-            + address.kana2
-            + address.kana3
-            + ")"
+                 cell.textLabel?.text = "\(address.address())(\(address.kana()))"
+
         return cell
     }
     
